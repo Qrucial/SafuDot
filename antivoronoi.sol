2fd390b6cfff6b13f522abacac1f7bdb9269ea9e
six <51x@keemail.me>
2022-01-26 00:43:26 +0100

    Beta version of the solidity code, fully functional.

--- antivoronoi.sol
+++ antivoronoi.sol
@@ -5,6 +5,7 @@ contract AntiVoronoiNFT {
     address public admin;
     uint256 public maxNFTs;
     uint256 public NFTCount;
+    uint256 public NFTPrice;
     mapping (uint256 => string) internal idToHash;
     mapping (uint256 => address) internal idToOwner;
     uint256 public blockTime;
@@ -13,15 +14,20 @@ contract AntiVoronoiNFT {
         admin = msg.sender;
         maxNFTs = 99;
         NFTCount = 0;
+        NFTPrice = 10000000000000000;
         blockTime = block.timestamp;
     }
     
-    function mint() external {
+    function mint(string memory _hashu) external payable {
         require(msg.sender == admin, 'You are not the central admin!');
         require(blockTime <= block.timestamp + 3 days, 'Chill bro!');
+        require(NFTCount <= 99, 'You shall not pass! All NFTz are minted!');
+        require(msg.value >= NFTPrice, 'Where are da fundz?');
         blockTime = block.timestamp;
-        maxNFTs = maxNFTs + 1;
+        NFTCount = NFTCount + 1;
+        NFTPrice = NFTPrice * 2;
         idToOwner[NFTCount] = msg.sender;
+        idToHash[NFTCount] = _hashu;
     }
 
     function transfer(uint256 _tokenId, address _toAddress) external {
@@ -36,6 +42,16 @@ contract AntiVoronoiNFT {
 
     function WhoGotchaThat(uint256 _whichOne) public view returns (address) {
         return(idToOwner[_whichOne]);
+    }
+
+    function WhatIsTheHash(uint256 _tokenId) public view returns (string memory){
+        return(idToHash[_tokenId]);
+    }
+
+    function adminChange(address _newAdmin) external returns (bool) {
+        require(blockTime <= block.timestamp + 66 minutes, 'Welcome to the game!');
+        admin = _newAdmin;
+        return true;
     }
 
     fallback() external payable {}
