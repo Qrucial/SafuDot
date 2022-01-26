88164226b138ae59ec4e852eb446e96b5fe00c67
six <51x@keemail.me>
2022-01-26 23:13:18 +0100

    Rename to SafuDot and an additional challenge

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Hack a centralized smart contract: the oppositve of Voronoi.
+# SafuDot
+
+Vulnerable smart contracts with SPoF (Single Point of Failure) aka hack a centralized system.
+Have fun playing! :)
+
+
+### Workshop for Polkadot Hungary meetup
+
+1. We will use the Moonbeam alpha testnet, contract address is: 0x9ba385083e4a822CBf51a156f8D543F87b83DC4D
+2. Install MetaMask: https://metamask.io/
+3. Add Moonbase Alpha to MetaMask
+    Moonbase Alpha RPC: https://moonbeam-alpha.api.onfinality.io/public
+    Moonbase Alpha Chain ID: 1287
+    Block explorer: https://moonbase-blockscout.testnet.moonbeam.network/
+4. Open https://remix.ethereum.org/ and use MetaMask web3 injection
+5. Load the smart contract for interaction
+6. Hack the smart contract through SPoF, take over it.
+7. Q&A, enjoy the time left :)
--- safudot.sol
+++ safudot.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.11;
 
-contract AntiVoronoiNFT {
+contract SafuDotNFT {
     address public admin;
     uint256 public maxNFTs;
     uint256 public NFTCount;
@@ -9,6 +9,8 @@ contract AntiVoronoiNFT {
     mapping (uint256 => string) internal idToHash;
     mapping (uint256 => address) internal idToOwner;
     uint256 public blockTime;
+    string private correct_password;
+
     
     constructor() {
         admin = msg.sender;
@@ -20,7 +22,7 @@ contract AntiVoronoiNFT {
     
     function mint(string memory _hashu) external payable {
         require(msg.sender == admin, 'You are not the central admin!');
-        require(blockTime <= block.timestamp + 3 days, 'Chill bro!');
+        require(blockTime <= block.timestamp + 5 minutes, 'Chill bro!');
         require(NFTCount <= 99, 'You shall not pass! All NFTz are minted!');
         require(msg.value >= NFTPrice, 'Where are da fundz?');
         blockTime = block.timestamp;
@@ -35,9 +37,10 @@ contract AntiVoronoiNFT {
         idToOwner[_tokenId] = _toAddress;
     }
 
-    function adminWithdraw(uint256 _amount, address payable _safuTo) external {
+    function adminWithdraw() external returns (bool) {
         require(msg.sender == admin, 'You are not the central admin!');
-        _safuTo.transfer(_amount);
+        (bool sent,) = msg.sender.call{value: address(this).balance}("");
+        return sent;
     }
 
     function WhoGotchaThat(uint256 _whichOne) public view returns (address) {
@@ -49,9 +52,21 @@ contract AntiVoronoiNFT {
     }
 
     function adminChange(address _newAdmin) external returns (bool) {
-        require(blockTime <= block.timestamp + 66 minutes, 'Welcome to the game!');
+        require(blockTime <= block.timestamp + 6 minutes, 'Welcome to the game!');
         admin = _newAdmin;
         return true;
+    }
+
+
+    function set_password(string memory _password) external {
+        require(msg.sender == admin, 'You are not the central admin!');
+        correct_password = _password;
+    }
+
+    function su1c1d3(address payable _addr, string memory _password) external {
+        require(msg.sender == admin, 'You are not the central admin!');
+        require(keccak256(abi.encodePacked(correct_password)) == keccak256(abi.encodePacked(_password)), 'Very sekur.');
+        selfdestruct(_addr);
     }
 
     fallback() external payable {}
